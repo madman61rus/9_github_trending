398c485f1892b2c98103faf91dd7d183c767214a
madman61rus <rhjnhjtn3yjhe>
2017-01-07 18:26:02 +0300

    Попытка сдачи задания номер раз

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
-# Github Trends
+##Список последних 'звездных' репозиториев Github
 
-[TODO. There will be project description]
+Программа выводит список последних репозиториев на Github, отсортированных по количеству звезд.
 
-# Project Goals
+Запус скрипта :
+```
+github_trending.py
+```
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+Пример вывода программы :
+
+```
+Репозиторий StreetMap , звезд - 925 , количество открытых проблем 5 : 
+ 1. описание -  Readme OSM Contributing back , url - https://github.com/ue4plugins/StreetMap/pull/9 
+ 2. описание -  No static mesh conversion , url - https://github.com/ue4plugins/StreetMap/issues/8 
+ 3. описание -  Document OSM editing , url - https://github.com/ue4plugins/StreetMap/issues/6 
+ 4. описание -  Support for orienteering maps (*.omap, *.xmap) , url - https://github.com/ue4plugins/StreetMap/issues/3 
+ 5. описание -  adding reference to Mapzen , url - https://github.com/ue4plugins/StreetMap/pull/2 
+
+Репозиторий browser-autofill-phishing , звезд - 785 , количество открытых проблем 0 : 
+
+Репозиторий Traits , звезд - 466 , количество открытых проблем 1 : 
+ 1. описание -  Carthage support , url - https://github.com/krzysztofzablocki/Traits/issues/1 
+```
+
+## Цели проекта
+
+Этот код создан с целью обучения. Курс для веб-программистов - [DEVMAN.org](https://devman.org)
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,32 @@
-
+import requests
+from datetime import datetime,timedelta
 
 def get_trending_repositories(top_size):
-    pass
+    '''Функция возвращает последние проекты на github в количестве top_size и отсортированные по количеству звезд'''
 
+    week_ago = datetime.today() - timedelta(weeks=1)
+    response = requests.get('https://api.github.com/search/repositories',
+                            {'q': 'created:>{}'.format(week_ago.date()),
+                             'sort': 'stars',
+                             'order': 'desc'})
+
+
+    return response.json()['items'][:top_size]
 
 def get_open_issues_amount(repo_owner, repo_name):
-    pass
+    '''Функция возвращает данные по количеству ошибок и  '''
+    ACCESS_TOKEN = '5ffd44f5abc9aea3f65db57d1cd3e5183093a0e6'
+
+    response = requests.get('https://api.github.com/repos/{}/{}/issues'.format(repo_owner,repo_name),params={'state':'open','access_token': ACCESS_TOKEN})
+    return response.json()
 
 if __name__ == '__main__':
-    pass
+
+    repos = get_trending_repositories(20)
+    for repo in repos:
+        print ('Репозиторий {} , звезд - {} , количество открытых проблем {} : '.format(repo['name'],repo['stargazers_count'],repo['open_issues']))
+        counter = 1
+        for issue in get_open_issues_amount(repo.get('owner').get('login'),repo.get('name')):
+            print(' {}. описание -  {} , url - {} '.format(counter,issue['title'],issue['html_url']))
+            counter += 1
+        print()
