aa4752402af56e9bc776f8b2e0d24f27259ddfe1
madman61rus <rhjnhjtn3yjhe>
2017-01-07 18:29:36 +0300

    + Перенес параметры в запросе

--- github_trending.py
+++ github_trending.py
@@ -5,10 +5,10 @@ def get_trending_repositories(top_size):
     '''Функция возвращает последние проекты на github в количестве top_size и отсортированные по количеству звезд'''
 
     week_ago = datetime.today() - timedelta(weeks=1)
-    response = requests.get('https://api.github.com/search/repositories',
-                            {'q': 'created:>{}'.format(week_ago.date()),
+    params = {'q': 'created:>{}'.format(week_ago.date()),
                              'sort': 'stars',
-                             'order': 'desc'})
+                             'order': 'desc'}
+    response = requests.get('https://api.github.com/search/repositories',params)
 
 
     return response.json()['items'][:top_size]
@@ -16,8 +16,8 @@ def get_trending_repositories(top_size):
 def get_open_issues_amount(repo_owner, repo_name):
     '''Функция возвращает данные по количеству ошибок и  '''
     ACCESS_TOKEN = '5ffd44f5abc9aea3f65db57d1cd3e5183093a0e6'
-
-    response = requests.get('https://api.github.com/repos/{}/{}/issues'.format(repo_owner,repo_name),params={'state':'open','access_token': ACCESS_TOKEN})
+    params = {'state':'open','access_token': ACCESS_TOKEN}
+    response = requests.get('https://api.github.com/repos/{}/{}/issues'.format(repo_owner,repo_name), params)
     return response.json()
 
 if __name__ == '__main__':
