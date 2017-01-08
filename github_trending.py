993390100c8b1243a5b6b86944b6c3f92acb8f33
madman61rus <rhjnhjtn3yjhe>
2017-01-08 13:01:46 +0300

    + Перенес вывод в отдельную функцию + Магические константы перенес в переменные + В функции get_trending_repositories добавил переменную weeks в замен маг. константы 1 + Переименовал переменные в get_issues_amount

--- github_trending.py
+++ github_trending.py
@@ -1,10 +1,9 @@
 import requests
 from datetime import datetime,timedelta
 
-def get_trending_repositories(top_size):
-    '''Функция возвращает последние проекты на github в количестве top_size и отсортированные по количеству звезд'''
+def get_trending_repositories(top_size,weeks):
 
-    week_ago = datetime.today() - timedelta(weeks=1)
+    week_ago = datetime.today() - timedelta(weeks)
     params = {'q': 'created:>{}'.format(week_ago.date()),
                              'sort': 'stars',
                              'order': 'desc'}
@@ -14,19 +13,25 @@ def get_trending_repositories(top_size):
     return response.json()['items'][:top_size]
 
 def get_open_issues_amount(repo_owner, repo_name):
-    '''Функция возвращает данные по количеству ошибок и  '''
     access_token = '5ffd44f5abc9aea3f65db57d1cd3e5183093a0e6'
     params = {'state':'open','access_token': access_token}
     response = requests.get('https://api.github.com/repos/{}/{}/issues'.format(repo_owner,repo_name), params)
-    return response.json()
+    issues = response.json()
+    return issues
 
-if __name__ == '__main__':
-
-    repos = get_trending_repositories(20)
+def print_to_console(repos):
     for repo in repos:
-        print ('Репозиторий {} , звезд - {} , количество открытых проблем {} : '.format(repo['name'],repo['stargazers_count'],repo['open_issues']))
+        print('Репозиторий {} , звезд - {} , количество открытых проблем {} : '.format(repo['name'],
+                                                                                       repo['stargazers_count'],
+                                                                                       repo['open_issues']))
         counter = 1
-        for issue in get_open_issues_amount(repo.get('owner').get('login'),repo.get('name')):
-            print(' {}. описание -  {} , url - {} '.format(counter,issue['title'],issue['html_url']))
+        for issue in get_open_issues_amount(repo.get('owner').get('login'), repo.get('name')):
+            print(' {}. описание -  {} , url - {} '.format(counter, issue['title'], issue['html_url']))
             counter += 1
         print()
+
+if __name__ == '__main__':
+    top_size=20
+    weeks=1
+    repos = get_trending_repositories(top_size,weeks)
+    print_to_console(repos)
